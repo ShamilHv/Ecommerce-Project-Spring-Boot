63b59f4b0151e4754f18469a45cc550abc705ddb
Shamil <samilheyderov2003@gmail.com>
2025-01-11 10:23:54 +0400

    Ordering and Payment have been implemented.

--- src/main/java/com/ecommerce/project/service/impl/CartServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/impl/CartServiceImpl.java
@@ -97,15 +97,23 @@ public class CartServiceImpl implements CartService {
         List<Cart> carts = cartRepository.findAll();
 
         if (carts.size() == 0) {
-            throw new APIException("No cart found");
+            throw new APIException("No cart exists");
         }
 
         List<CartDTO> cartDTOs = carts.stream().map(cart -> {
             CartDTO cartDTO = modelMapper.map(cart, CartDTO.class);
-            List<ProductDTO> products = cart.getCartItems().stream().map(p -> modelMapper.map(p.getProduct(), ProductDTO.class)).collect(Collectors.toList());
+
+            List<ProductDTO> products = cart.getCartItems().stream().map(cartItem -> {
+                ProductDTO productDTO = modelMapper.map(cartItem.getProduct(), ProductDTO.class);
+                productDTO.setQuantity(cartItem.getQuantity());
+                return productDTO;
+            }).collect(Collectors.toList());
+
 
             cartDTO.setProducts(products);
+
             return cartDTO;
+
         }).collect(Collectors.toList());
 
         return cartDTOs;
